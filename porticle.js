b6a2217042b2ed3c68db240bd0578af99adc8b76
CodHeK <gaganganapathyas@yahoo.in>
2018-12-04 05:48:42 +0530

    updated mono

--- porticle.js
+++ porticle.js
@@ -4,9 +4,23 @@ setTimeout(print, 500);
 
 function porticle(numParts, numSize, type) {
   if(type === "mono") {
-    for(var i=1;i<=numParts;i++) {
+    for(var i=1;i<=numParts/2;i++) {
       var dot = document.createElement('div');
       dot.classList.add('dot2');
+      dot.style.height = Math.floor(Math.random()*(numSize) + 1);
+      dot.style.width = dot.style.height;
+      dot.style.left = Math.floor(Math.random()*(2000) + 0)
+      dot.style.top = Math.floor(Math.random()*(2000) + 0)
+      dot.style.opacity = Math.random()*(1) + 0;
+      dot.style.color = '#000';
+      dot.style.border = `1px solid ${dot.style.color}`;
+      dot.style.backgroundColor = dot.style.color;
+      dot.style.ZIndex = Math.random()*(1000) - 100;
+      dots.push(dot);
+    }
+    for(var i=1;i<=numParts/2;i++) {
+      var dot = document.createElement('div');
+      dot.classList.add('dot1');
       dot.style.height = Math.floor(Math.random()*(numSize) + 1);
       dot.style.width = dot.style.height;
       dot.style.left = Math.floor(Math.random()*(2000) + 0)
